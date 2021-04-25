3fc6b51d8028708910128a830a374b5e178beb5b
Matt Chodaczek <mchodac1@jhu.edu>
2021-04-25 17:05:31 -0400

    added displayArt test

---
+++ src/test/java/main/PokemonBattleSimulatorTest.java
@@ -0,0 +1,37 @@
+package main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PokemonBattleSimulatorTest {
+    private PokemonBattleSimulator pbs;
+
+    @BeforeEach
+    public void createSim() {
+        pbs = new PokemonBattleSimulator();
+    }
+
+    @Test
+    public void testDisplayArt() throws IOException {
+        String expected = "\r\n" +
+                String.join("\r\n", Files.readAllLines(Paths.get("data/art/mcharizardxoff-60.txt"), StandardCharsets.UTF_8)) +
+                "\r\n";
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outStream));
+        pbs.displayArt();
+        String actual = outStream.toString();
+        System.setOut(System.out);
+        assertEquals(expected, actual);
+    }
+
+    // TODO figure how to mock (or somehow simulate) user input to wbox test runSimulator()
+}
